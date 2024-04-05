da6dbc6c175590d77f0ae0aa37a666cfe8cdd3d8
MarkusT22 <markus.tammoja@tktk.ee>
2024-04-05 11:41:47 +0300

    add week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//FOR-LOOP
+//Dry-principle - do not repeat yourself
+
+int i = 0;
+
+for( i = 0; i < 5; i++) //i++ on sama asi, mis i+1
+{
+    Console.WriteLine("We don't need no education-");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Hace a nice day!");
